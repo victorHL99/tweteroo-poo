0fbbe9847d8f1d0545e643c855f5c916f983a352
victorHL99 <victor.lessa@estudante.ufla.br>
2022-11-28 15:48:44 -0300

    feat: :sparkles: create router for tweets with POO

---
+++ back-end/src/routes/tweetsRouter.js
@@ -0,0 +1,29 @@
+import { Router } from "express";
+import { TweetsController } from "../controller/tweetsContoller.js";
+
+const tweetsRouter = Router();
+
+tweetsRouter
+  .post("/tweets", (req, res) => {
+    const { tweet, username } = req.body;
+
+    const data = new TweetsController().createTweet(tweet, username);
+
+    res.status(data.status).send(data.message);
+  })
+  .get("/tweets", (req, res) => {
+    const { page } = req.query;
+
+    const data = new TweetsController().getTweetsByOrder(page);
+
+    res.status(data.status).send(data.message);
+  })
+  .get("/tweets/:username", (req, res) => {
+    const { username } = req.params;
+
+    const data = new TweetsController().getTweetsByName(username);
+
+    res.status(data.status).send(data.message);
+  });
+
+export { tweetsRouter };
