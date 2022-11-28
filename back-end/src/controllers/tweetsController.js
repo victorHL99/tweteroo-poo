42055ad0198e1677019669f6cf7d460875432d99
victorHL99 <victor.lessa@estudante.ufla.br>
2022-11-28 15:50:30 -0300

    feat: :sparkles: create controller for tweets with POO

---
+++ back-end/src/controllers/tweetsController.js
@@ -0,0 +1,15 @@
+import { TweetsServices } from "../services/tweetsServices.js";
+
+export class TweetsController {
+  createTweet(tweet, username) {
+    return new TweetsServices().createTweet(tweet, username);
+  }
+
+  getTweetsByOrder(page) {
+    return new TweetsServices().getTweetsByOrder(page);
+  }
+
+  getTweetsByName(username) {
+    return new TweetsServices().getTweetsByName(username);
+  }
+}
