603eff608b5c580f62da1f49c869e2ee6811bd62
victorHL99 <victor.lessa@estudante.ufla.br>
2022-11-28 15:37:23 -0300

    feat: :sparkles: create app.js with POO

---
+++ back-end/app.js
@@ -0,0 +1,23 @@
+import cors from "cors";
+import express, { json } from "express";
+import router from "./routes/index.js";
+
+class App {
+  constructor(port) {
+    const app = (this.expressApp = express());
+
+    app.disable("x-powered-by");
+    app.use(cors());
+    app.use(json());
+
+    this.setRoutes();
+
+    app.listen(port, () => console.log(`Server running on port ${port}`));
+  }
+
+  setRoutes() {
+    this.expressApp.use("/", router);
+  }
+}
+
+export default App;
